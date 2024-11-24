4a5f1ba7f871af166b4b2fd673b4f7b5a93b6743
ST-DDT <ST-DDT@gmx.de>
2024-11-24 01:32:55 +0100

    test: add snapshot test for all locales' character sets

--- test/locale-data.spec.ts
+++ test/locale-data.spec.ts
@@ -16,6 +16,102 @@ function checkLocaleData(data: unknown) {
   }
 }
 
+/**
+ * A set of locale data paths that should be ignored when checking for characters.
+ * Because these usually contain data using Latin characters that are otherwise not relevant to the locale.
+ */
+const ignoredCharacterData = new Set([
+  '.airline.airline[].iataCode',
+  '.airline.airplane[].iataTypeCode',
+  '.airline.airport[].iataCode',
+  '.finance.currency',
+  '.finance.credit_card',
+  '.hacker.abbreviation',
+  '.internet.domain_suffix',
+  '.internet.emoji',
+  '.internet.example_email',
+  '.internet.free_email',
+  '.location.country_code',
+  '.metadata',
+  '.science.chemical_element[].symbol',
+  '.science.unit[].symbol',
+  '.system.directory_path',
+  '.system.mime_type',
+]);
+
+function uniqueCharacters(data: string | string[]): string[] {
+  return [...new Set(data)];
+}
+
+function allCharacters(data: unknown, path: string = ''): string[] {
+  if (ignoredCharacterData.has(path)) {
+    return [];
+  } else if (Array.isArray(data)) {
+    return uniqueCharacters(data.flatMap((e) => allCharacters(e, `${path}[]`)));
+  } else if (typeof data === 'object' && data != null) {
+    return uniqueCharacters(
+      Object.entries(data).flatMap(([key, entry]) =>
+        allCharacters(entry, `${path}.${key}`)
+      )
+    );
+  } else if (typeof data === 'string') {
+    return uniqueCharacters(
+      data
+        .replaceAll(/{{(?:[^{]|{(?!{))*?}}/g, '') // remove placeholders
+        .replaceAll(/\d+/g, '') // remove numbers
+    );
+  }
+
+  return [];
+}
+
 describe('locale-data', () => {
   checkLocaleData(allLocales);
+
+  // This test exists to keep track of the characters used in each locale.
+  // It doesn't matter if new characters are added as long as they belong to that language.
+  it('should only have known characters', () => {
+    const characterSets = Object.fromEntries(
+      Object.entries(allLocales).map(([locale, data]) => [
+        locale,
+        allCharacters(data).sort().join(''),
+      ])
+    );
+    expect(characterSets).toMatchSnapshot();
+  });
 });
+
+/**
+ * Helper function to locate a specific character in the locale data.
+ *
+ * @param data The locale data to search in.
+ * @param char The character to search for.
+ * @param path The current path in the data. Defaults to an empty string.
+ *
+ * @example
+ * locateCharacter(allLocales.zh_CN, 'A');
+ */
+export function locateCharacter(
+  data: unknown,
+  char: string,
+  path: string = ''
+): void {
+  if (ignoredCharacterData.has(path)) {
+    return;
+  }
+
+  if (Array.isArray(data)) {
+    for (const e of data) {
+      locateCharacter(e, char, `${path}[]`);
+    }
+  } else if (typeof data === 'object' && data != null) {
+    for (const [key, entry] of Object.entries(data)) {
+      locateCharacter(entry, char, `${path}.${key}`);
+    }
+  } else if (
+    typeof data === 'string' &&
+    allCharacters(data, path).includes(char)
+  ) {
+    console.log(path, data);
+  }
+}
