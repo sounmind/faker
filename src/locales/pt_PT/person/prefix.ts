2a93a0d0001c2e3b2456ce13585d1894c0a6f799
ST-DDT <ST-DDT@gmx.de>
2024-11-24 21:32:50 +0100

    chore: restore comments on pt_PT prefixes

--- src/locales/pt_PT/person/prefix.ts
+++ src/locales/pt_PT/person/prefix.ts
@@ -1,31 +1,40 @@
 export default {
   generic: [
-    'Adv.',
-    'Adv.ª',
-    'Arq.',
-    'Arq.ª',
-    'Dr.',
-    'Dra.',
-    'Enf.',
-    'Enf.ª',
-    'Eng.ª',
-    'Eng.º',
-    'Prof.',
-    'Prof.ª',
-    'Sr.',
-    'Sra.',
-    'Téc.',
-    'Téc.ª',
+    'Adv.', // Advogado (Lawyer, male)
+    'Adv.ª', // Advogada (Lawyer, female)
+    'Arq.', // Arquiteto (Architect, male)
+    'Arq.ª', // Arquiteta (Architect, female)
+    'Dr.', // Doutor (Doctor, male)
+    'Dra.', // Doutora (Doctor, female)
+    'Enf.', // Enfermeiro (Nurse, male)
+    'Enf.ª', // Enfermeira (Nurse, female)
+    'Eng.ª', // Engenheira (Engineer, female)
+    'Eng.º', // Engenheiro (Engineer, male)
+    'Prof.', // Professor (male)
+    'Prof.ª', // Professora (female)
+    'Sr.', // Senhor (Mr.)
+    'Sra.', // Senhora (Mrs.)
+    'Téc.', // Técnico (Technician, male)
+    'Téc.ª', // Técnica (Technician, female)
   ],
   female: [
-    'Adv.ª',
-    'Arq.ª',
-    'Dra.',
-    'Enf.ª',
-    'Eng.ª',
-    'Prof.ª',
-    'Sra.',
-    'Téc.ª',
+    'Adv.ª', // Advogada (Lawyer, female)
+    'Arq.ª', // Arquiteta (Architect, female)
+    'Dra.', // Doutora (Doctor, female)
+    'Enf.ª', // Enfermeira (Nurse, female)
+    'Eng.ª', // Engenheira (Engineer, female)
+    'Prof.ª', // Professora (female)
+    'Sra.', // Senhora (Mrs.)
+    'Téc.ª', // Técnica (Technician, female)
   ],
-  male: ['Adv.', 'Arq.', 'Dr.', 'Enf.', 'Eng.º', 'Prof.', 'Sr.', 'Téc.'],
+  male: [
+    'Adv.', // Advogado (Lawyer, male)
+    'Arq.', // Arquiteto (Architect, male)
+    'Dr.', // Doutor (Doctor, male)
+    'Enf.', // Enfermeiro (Nurse, male)
+    'Eng.º', // Engenheiro (Engineer, male)
+    'Prof.', // Professor (male)
+    'Sr.', // Senhor (Mr.)
+    'Téc.', // Técnico (Technician, male)
+  ],
 };
